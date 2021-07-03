554c9ae601dbb10679bedee530f32a62754cba4a
surajseth90 <surajseth90@gmail.com>
2021-07-03 20:10:38 +0530

    Added code to show enterend data in list

--- src/component/AddContact.js
+++ src/component/AddContact.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react'
 
 export class AddContact extends Component {
+    state={
+        name:"",
+        email:""
+         
+    }
+
+    save=(event)=>{
+        event.preventDefault();
+        if(this.state.name==="" || this.state.email===""){
+            alert("ALL fields are required");
+            return;
+        }
+        this.props.addContactHandler(this.state);
+        this.setState({name:"",email:""});
+
+    }
     render() {
         return (
             <div className="ui main">
                 <h2>Add Contact</h2>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.save}>
                     <div className="field">
                         <label>
                             Name
                         </label>
-                        <input type="text" name="name" placeholder="Name"></input>
+                        <input type="text" name="name" value={this.state.name} placeholder="Name" onChange={(event)=>this.setState({name:event.target.value})}/>
                         <label>
                             Name
                         </label>
-                        <input type="email" name="email" placeholder="Email"></input>
+                        <input type="email" name="email" value={this.state.email} placeholder="Email" onChange={(event)=>this.setState({email:event.target.value})}/>
 
                     </div>
                     <button className="ui button yellow">Add</button>
--- src/component/App.js
+++ src/component/App.js
@@ -1,27 +1,20 @@
 import './App.css';
-import React from 'react';
+import React,{useState} from 'react';
 import Header from './Header';
 import AddContact from './AddContact';
 import {ContactList} from './ContactList';
 
 function App() {
   
-  const contacts=[
-    {
-      id:"1",
-      name:"Suraj",
-      email:"suraj@gmail.com"
-    },
-    {
-      id:"2",
-      name:"Sonali",
-      email:"suraj@gmail.com"
-    }
-  ]
+  const[contacts,setContacts] = useState([]);
+
+  const addContactHandler =(contact)=>{
+    setContacts([...contacts,contact]);
+  }
   return (
     <>
     <Header />
-    <AddContact/>
+    <AddContact addContactHandler={addContactHandler}/>
     <ContactList contacts={contacts}/>
     </>
   );
