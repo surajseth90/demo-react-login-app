d7badb550ea9e37e99bc5d4cc11d043060eae77e
surajseth90 <surajseth90@gmail.com>
2021-07-03 20:29:30 +0530

    Added data in local storage

--- src/component/App.js
+++ src/component/App.js
@@ -1,21 +1,33 @@
 import './App.css';
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import AddContact from './AddContact';
-import {ContactList} from './ContactList';
+import { ContactList } from './ContactList';
 
 function App() {
-  
-  const[contacts,setContacts] = useState([]);
 
-  const addContactHandler =(contact)=>{
-    setContacts([...contacts,contact]);
+  const key = "contacts";
+
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, contact]);
   }
+
+  useEffect(() => {
+    let list = JSON.parse(localStorage.getItem(key, contacts));
+    if (list) setContacts(list);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <>
-    <Header />
-    <AddContact addContactHandler={addContactHandler}/>
-    <ContactList contacts={contacts}/>
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
     </>
   );
 }
